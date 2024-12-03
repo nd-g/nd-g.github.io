aecb143a219f77244fa6b8a8ddc22356dad5a6d3
nd-g <95657502+nd-g@users.noreply.github.com>
2024-12-03 00:18:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Firebase Initialization
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBkb-WBCHIa6DCEuMzELE4FBsYwKhR5fAw",
+  authDomain: "date-a-deafie.firebaseapp.com",
+  projectId: "date-a-deafie",
+  storageBucket: "date-a-deafie.firebasestorage.app",
+  messagingSenderId: "1028508036545",
+  appId: "1:1028508036545:web:cb58d6825235b476761340"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
